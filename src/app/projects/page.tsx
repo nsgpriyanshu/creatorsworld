ceebb63e516f2c4d36cf351d61b6b0df649f62b5
ŊʂƓ PRIYANSHU <166146782+nsgpriyanshu@users.noreply.github.com>
2024-05-09 16:09:33 +0530

    Core projects
    Added projects folder

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1>Coming Soon Projects Page</h1>
+    </main>
+  )
+}
