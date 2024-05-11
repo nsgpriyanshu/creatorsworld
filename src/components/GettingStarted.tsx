4a1e90b82d0bc79b1f3de9b20f188c9ec8de0388
ŊʂƓ PRIYANSHU <166146782+nsgpriyanshu@users.noreply.github.com>
2024-05-11 08:13:51 +0530

    Update GettingStarted.tsx
    Updated version

--- src/components/GettingStarted.tsx
+++ src/components/GettingStarted.tsx
@@ -3,7 +3,10 @@ import { PinContainer } from './ui/3d-pin'
 function GettingStarted() {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-      <h1 className="mt-5 md:mt-0 text-2xl md:text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+      <h1
+        className="mt-5 md:mt-0 text-2xl md:text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
+        id="getting-started"
+      >
         Getting Started
       </h1>
       <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
