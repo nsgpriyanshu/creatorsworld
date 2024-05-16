869e5f1f8ef59a667da3acad08a61a23f2878a5b
ŊʂƓ PRIYANSHU <166146782+nsgpriyanshu@users.noreply.github.com>
2024-05-16 09:48:39 +0530

    Update TestimonialCards.tsx
    Fixed a issue

--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -33,7 +33,7 @@ function Testimonialcards() {
   return (
     <div className="h-[35rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
       <h2 className="text-center text-xl md:text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
-        Creator Chronicles: Voices of our Creators
+        Creator Chronicles: Voices of our user Creators
       </h2>
       <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-6xl">
