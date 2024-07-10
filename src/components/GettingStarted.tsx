853216344e0c11481df43f97164b0cb7f4e2b5bc
ŊʂƓ PRIYANSHU <166146782+nsgpriyanshu@users.noreply.github.com>
2024-07-10 16:15:50 +0530

    fix: image paths

--- src/components/GettingStarted.tsx
+++ src/components/GettingStarted.tsx
@@ -32,7 +32,7 @@ function GettingStarted() {
             </div>
             <div className="mt-4 flex w-full flex-1 overflow-hidden rounded-lg bg-gradient-to-br from-green-500 via-teal-500 to-cyan-500">
               <Image
-                src="/icons/colourfull-pfp.jpg"
+                src="./icons/colourfull-pfp.jpg"
                 width={800}
                 height={800}
                 alt="colourfull-pfp"
@@ -56,7 +56,7 @@ function GettingStarted() {
             </div>
             <div className="mt-4 flex w-full flex-1 overflow-hidden rounded-lg bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500">
               <Image
-                src="/demos/shadowfightlogo.jpg"
+                src="./demos/shadowfightlogo.jpg"
                 width={600}
                 height={600}
                 alt="colourfull-pfp"
