2fd9573378bc3dcd0cb68fb6fcd1f8342f8f4b14
ŊʂƓ PRIYANSHU <166146782+nsgpriyanshu@users.noreply.github.com>
2024-07-12 13:27:42 +0530

    fix: image issue #10

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: '/creatorsworld',
+  assetPrefix: '/creatorsworld/', 
+  images: {
+    loader: 'default',
+    path: '/creatorsworld/_next/image',
+  },
+}
 
 export default nextConfig
