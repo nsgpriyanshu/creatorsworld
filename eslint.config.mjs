af6c46581c4de140420eb75a3bbdb00b097d5bbd
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2025-12-10 17:50:36 +0530

    feat: upgrade to nextjs 16, and improve ui/ux

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,23 @@
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
+import nextTypescript from 'eslint-config-next/typescript'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
-
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-
+  ...nextCoreWebVitals,
+  ...nextTypescript,
   // Add this override to disable the rules globally
   {
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
     },
+  },
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
 ]
 
