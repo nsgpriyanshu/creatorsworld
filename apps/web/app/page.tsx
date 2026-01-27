26d61a95e3f8803e4df1871cf6a7920ab2c93dfe
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2026-01-27 14:53:07 +0530

    feat: init for production

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,9 @@
-import { Button } from "@repo/ui/components/ui/button"
+import { Button } from "@repo/ui/components/ui/button";
 
 export default function Page() {
   return (
-    <main>
+    <main className="h-screen w-full justify-center items-center">
+      <h1 className="text-4xl">Hi Prakriti Lisaa</h1>
       <Button>Click me</Button>
     </main>
   );
